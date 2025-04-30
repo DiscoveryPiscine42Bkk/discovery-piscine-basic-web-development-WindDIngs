8c6d072d13b5ecc66576b20a1c5d2011c0d13e7b
Patchpakintr Chiwkha <pchiwkha@z2t2c4.42bangkok.com>
2025-04-30 22:28:03 +0700

    ex02b

---
+++ cell03/ex04/ex02bis/calc.js
@@ -0,0 +1,36 @@
+setInterval(()=>{alert("Please, use me...")},30000)
+function operation(operator,n1,n2){
+    n1 = Number(n1)
+    n2 = Number(n2)
+    if (operator == "+") {
+        return n1 + n2
+    } else if (operator == "-"){
+        return n1 - n2
+    } else if (operator == "*") {
+        return n1*n2
+    } else if (operator == "/") {
+        if (n2==0) {
+            return "It's over 9000"
+        } else{
+            return n1/n2
+        }
+    } else if (operator == "%") {
+        if (n2==0) {
+            return "It's over 9000"
+        } else{
+            return n1%n2
+        }
+}
+}
+
+function calc() {
+    var val1 = document.getElementById("inp1").value
+    var val2 = document.getElementById("inp2").value
+    if (!isNaN(val1)&&!isNaN(val2) && val1>=0 && val2>=0){
+        alert(operation(document.getElementById("Op").value,val1,val2))
+        console.log(operation(document.getElementById("Op").value,val1,val2))
+    } else {
+        alert("Error :(")
+        console.log("Error :(")
+    }
+}
